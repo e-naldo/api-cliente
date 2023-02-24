e9421c1934941952d33909bba66179138c371606
e-naldo <88638321+e-naldo@users.noreply.github.com>
2023-02-24 13:33:41 -0300

    Initial commit

---
+++ src/test/java/dev/api/cliente/ApiClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.api.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
